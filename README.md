ee8ec708c3a25f8f4740ddb8daa20a02d10994d2
Daniel Leavitt <daniel.leavitt@gmail.com>
2013-10-07 20:22:24 -0700

    readme and tests

--- README.md
+++ README.md
@@ -1,30 +1,61 @@
 # Ansible YAML Inventory
 
-Ansible inventory script for defining inventory using YAML files.
+Ansible inventory script for defining inventory using YAML files. YAML is pretty! And readily parseable in a a variety of languages other than Python, unlike the default `.ini` files ansible uses.
+
+It's adapated from the YAML inventory script which used to be part of ansible but isn't anymore.
 
 ## Installation
 
-Add this line to your application's Gemfile:
-
-    gem 'yaml_inventory'
-
-And then execute:
-
-    $ bundle
-
-Or install it yourself as:
+Install with
 
     $ gem install yaml_inventory
 
 ## Usage
 
-  $ yaminv
-
-  TODO: talk about usage.
+Adds the `yaminv` command.
 
 For help:
 
     $ yaminv --help
+
+### With Ansible
+
+Create a `hosts.yml` file in your current dir, and use it like this
+
+    $ ansible-playbook -i $(which yaminv) playbook.yml
+
+When run with ansible-playbook it will always pick up the hosts.yml file in the current directory.
+
+### Hosts File Format
+
+```yaml
+
+# standalone hosts are okay
+- standalonehost1
+- standalonehost2
+
+# host with vars
+- host: hostwithvars
+  vars:
+    - var1: hostvar1
+    - var2: hostvar2
+
+# group with vars and hosts
+- group: group2
+  vars:
+    - groupvar1: groupvar1
+  hosts:
+    - ghost1
+    - ghost2
+  groups:
+    - group1
+
+# subgroup
+- group: group1
+  hosts:
+    - host3
+    - hostwithvars
+```
 
 ## Contributing
 
@@ -33,3 +64,7 @@ For help:
 3. Commit your changes (`git commit -am 'Add some feature'`)
 4. Push to the branch (`git push origin my-new-feature`)
 5. Create new Pull Request
+
+### Tests
+
+Run with `rake test`
--- spec/yaml_inventory_spec.rb
+++ spec/yaml_inventory_spec.rb
@@ -3,17 +3,147 @@ require "pry"
 require "pp"
 
 describe YAMLInventory do
-  let(:inventory) do 
+  let(:inventory) do
     YAMLInventory::Parser.new(YAML.load_file("spec/fixtures/hosts.yml"))
   end
 
   describe "#groups" do
-    it "doesn't explode too hard" do
-      inventory.groups
+    it "makes the right groups" do
+      groups = %w( group1 exsubgroup ungrouped )
+      inventory.groups.keys.sort.must_equal groups.sort
     end
+
+    describe "vars" do
+      it "supports setting vars" do
+        inventory.groups["group1"][:vars]["group1var1"].must_equal "group1var1"
+        inventory.groups["exsubgroup"][:vars]["exsubvar1"].must_equal "exsubvar1"
+      end
+
+      it "inherits vars from parent groups" do
+        inventory.groups["exsubgroup"][:vars]["group1var1"].must_equal "group1var1"
+      end
+
+      it "overrides vars inherited from parent groups" do
+        inventory.groups["group1"][:vars]["overriddenhg"].must_equal "overridden"
+        inventory.groups["exsubgroup"][:vars]["overriddenhg"].must_equal "overriddenbygroup"
+      end
+
+      it "does not inherit vars from child groups" do
+        inventory.groups["group1"][:vars]["exsubvar1"].must_be_nil
+      end
+
+      it "does not inherit vars from hosts" do
+        inventory.groups["group1"][:vars]["hostvar1"].must_be_nil
+        inventory.groups["group1"][:vars]["hostvar2"].must_be_nil
+      end
+
+      it "does not inherit vars from hosts" do
+        inventory.groups["group1"][:vars]["hostvar1"].must_be_nil
+        inventory.groups["group1"][:vars]["hostvar2"].must_be_nil
+      end
+    end
+
+    describe "hosts" do
+      it "supports setting undefined hosts" do
+        inventory.groups["group1"][:hosts].must_include "newhost"
+      end
+
+      it "supports setting previously-defined hosts" do
+        inventory.groups["group1"][:hosts].must_include "hostwithvars"
+        inventory.groups["exsubgroup"][:hosts].must_include "notstandalonehost"
+      end
+
+      it "encompasses hosts from child groups" do
+        inventory.groups["group1"][:hosts].must_include "notstandalonehost"
+      end
+
+      it "does not inherit hosts from parent groups" do
+        inventory.groups["exsubgroup"][:hosts].wont_include "newhost"
+        inventory.groups["exsubgroup"][:hosts].wont_include "hostwithvars"
+      end
+
+      it "allows defining groups within hosts" do
+        skip "Not implemented yet"
+      end
+
+      it "allows defining groups within groups" do
+        skip "Not implemented yet"
+      end
+    end
+
+    describe "groups" do
+      it "sets child groups" do
+        inventory.groups["group1"][:children].must_include "exsubgroup"
+      end
+    end
+
+    describe "'ungrouped' group" do
+      subject do
+        inventory.groups["ungrouped"]
+      end
+
+      let :standalones do
+        %w(standalonehost1 standalonehost2 standalonehostwithvars).sort
+      end
+
+      it "has hosts" do
+        subject[:hosts].wont_be_nil
+      end
+
+      it "contains only standalone hosts" do
+        subject[:hosts].sort.must_equal standalones
+      end
+
+      it "doesn't contain hosts that have groups" do
+        subject[:hosts].wont_include "notstandalonehost"
+      end
+
+      it "has no vars" do
+        subject[:vars].must_be_empty
+      end
+
+      it "has no children" do
+        subject[:children].must_be_empty
+      end
+    end
+
   end
 
   describe "#host" do
+    def host(hostname, var = nil)
+      h = inventory.host(hostname)
+      var ? h[var] : h
+    end
+
+    it "returns vars when they are set on hosts" do
+      host("hostwithvars", "hostvar1").must_equal "hostvar1v"
+      host("standalonehostwithvars", "hostvar3").must_equal "hostvar3v"
+    end
+
+    it "returns no vars for standalone hosts" do
+      host("standalonehost1").must_be_empty
+    end
+
+    it "inherits vars from groups" do
+      host("newhost", "group1var1").must_equal "group1var1"
+    end
+
+    it "inherits vars from parent groups" do
+      host("notstandalonehost", "group1var1").must_equal "group1var1"
+    end
+
+    it "overrides inherited vars with its own" do
+      host("hostwithvars", "overriddenh").must_equal "overriddenbyhost"
+    end
+
+    it "gives precedence to group vars from its most direct parent" do
+      host("notstandalonehost", "overriddenhg").must_equal "overriddenbygroup"
+    end
+
+    it "inherits group vars from ALL its parents" do
+      skip "No example yet"
+    end
+
     it "doesn't explode too hard" do
       inventory.host("hostwithvars")
     end
